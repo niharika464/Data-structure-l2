3c725db7d4a76b13bdaabaa0078b6e6a321a502b
niharika464 <niharikasinghgaur11@gmail.com>
2025-04-16 21:33:16 +0530

    data structure numbers

---
+++ ds.py
@@ -0,0 +1,9 @@
+def printnumber(n):
+    iteration=0
+    print("The number entered by the user is",n)
+    iteration+=1
+    print("Total iteration taken place-",iteration)
+
+printnumber(100) 
+printnumber(10) 
+print("\n with any value, the time taken by the code would not change...")      
