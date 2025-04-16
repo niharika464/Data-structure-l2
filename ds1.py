1adc0ae1e034a33d6a539ec5c427b5e1d3c7c4c5
niharika464 <niharikasinghgaur11@gmail.com>
2025-04-16 21:41:16 +0530

    ds part2

---
+++ ds1.py
@@ -0,0 +1,11 @@
+def ontime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print("When n is iterated, no. of time is:",iteration)
+
+
+ontime(20)
+ontime(10)    
+ontime(50)  
+print("Iteration for n no. of values will increase by each iteration...")      
